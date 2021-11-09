0d72704b1c2256f1af225fd16b3aea8d37522196
Stepan Kolodkin <zorokon@yandex.ru>
2021-11-09 10:44:36 +0300

    added filter cenzor

--- TestyPizzaBot.py
+++ TestyPizzaBot.py
@@ -1,8 +1,11 @@
+import string
+
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
 import os
+import json
 
 bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher(bot)
@@ -43,10 +46,10 @@ async def pizza_place_command(message: types.Message):
 
 @dp.message_handler()
 async def echo_send(message: types.Message):
-    if message.text == 'Привет':
-        await message.answer("И тебе привет!")
-    # await message.reply(message.text)
-    # await bot.send_message(message.from_user.id, message.text)
+    if {i.lower().translate(str.maketrans('', '', string.punctuation)) for i in message.text.split(' ')} \
+            .intersection(set(json.load(open('cenz.json')))) != set():
+        await message.reply('Маты запрещены')
+        await message.delete()
 
 
 executor.start_polling(dp, skip_updates=True, on_startup=on_start_up)
